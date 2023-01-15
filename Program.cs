c4a04de4136419b209fe6e65efa21ee6d0957177
Not Today <48966874+another-salad@users.noreply.github.com>
2023-01-15 15:56:50 +0000

    Initial commit, simple help text

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class MainClass
+{
+    static int Main(string[] args)
+    {
+        if (args.Length == 1 && HelpRequired(args[0]))
+        {
+            DisplayHelp();
+            return 1;
+        }
+        return 0;
+    }
+
+    private static bool HelpRequired(string param)
+    {
+        string normalizedParam = param.ToLower().Normalize();
+        return normalizedParam == "-h" || normalizedParam == "--help" || normalizedParam == "/?";
+    }
+
+    private static void DisplayHelp() {
+        string helpText = $"Command line params{Environment.NewLine}{Environment.NewLine}" +
+                          $"'-e': Encrypt the source file/directory{Environment.NewLine}" +
+                          $"'-d': Decrypt the source file/directory{Environment.NewLine}" +
+                          $"'-source': The source file/directory{Environment.NewLine}" +
+                          $"'-out': The output directory{Environment.NewLine}{Environment.NewLine}";
+        Console.Write(helpText);
+    }
+
+}
